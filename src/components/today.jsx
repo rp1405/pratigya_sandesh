f0ab15cac7c49f5880b6354c3cb5925959bd798f
rp1405 <rpareek140503@gmail.com>
2023-08-14 21:28:26 +0530

    final

--- src/components/today.jsx
+++ src/components/today.jsx
@@ -1,19 +1,24 @@
-import { useEffect, useState } from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import React, { useState, useEffect } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 import fullDate from "./date";
 import "./today.css";
 import { getDownloadURL, getStorage, listAll, ref } from "firebase/storage";
 import { storage } from "../firebase";
-const fileRef = ref(storage, "/copies/" + fullDate + "/");
+
 export default function Today() {
   const [images, setImages] = useState([]);
+  const fileRef = ref(storage, "/copies/" + fullDate + "/");
   useEffect(() => {
     listAll(fileRef).then((items) => {
-      // console.log(items);
       items.items.forEach((itemRef) => {
         getDownloadURL(itemRef).then((url) => {
-          setImages((prev) => [...prev, url]);
+          setImages((prev) => {
+            return [...prev, url];
+          });
+          console.log(images);
         });
       });
     });
@@ -25,7 +30,7 @@ export default function Today() {
       </div>
     );
   }
-  console.log(images);
+  // console.log(images);
   return (
     <div className="newspaper">
       <Carousel>{images.map(createSlide)}</Carousel>
